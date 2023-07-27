0372d1a99f2ec9f7df40b0414c01d2a63629e72e
stevanryan <stevanryan85@gmail.com>
2023-07-27 12:15:28 +0700

    adding class NotesService

---
+++ src/NotesService.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+class NotesService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getNotes(userId) {
+    const query = {
+      text: `SELECT notes.* FROM notes
+            LEFT JOIN collaborations ON collaborations.note_id = note.id
+            WHERE notes.owner = $1 OR collaborations.user_id = $1
+            GROUP BY notes.id`,
+      values: [userId],
+    };
+
+    const noteResult = await this._pool.query(query);
+
+    // Tidak perlu mapping karena tidak ditampilkan pada Apps sehingga properti bukan hal penting.
+    return noteResult.rows;
+  }
+}
+
+module.exports = NotesService;
