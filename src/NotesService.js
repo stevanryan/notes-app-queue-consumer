0410a7490e3bb189810f9c5d08eca70fe0a8a023
stevanryan <stevanryan85@gmail.com>
2023-07-27 15:07:09 +0700

    fixing typo on query

--- src/NotesService.js
+++ src/NotesService.js
@@ -8,7 +8,7 @@ class NotesService {
   async getNotes(userId) {
     const query = {
       text: `SELECT notes.* FROM notes
-            LEFT JOIN collaborations ON collaborations.note_id = note.id
+            LEFT JOIN collaborations ON collaborations.note_id = notes.id
             WHERE notes.owner = $1 OR collaborations.user_id = $1
             GROUP BY notes.id`,
       values: [userId],
